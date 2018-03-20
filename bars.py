eef5ee62f91477a5bfd5852d94e986757c801bdd
Ruslan <rusiacugap@mail.ru>
2018-03-20 21:04:19 +0300

    Начало работы

--- bars.py
+++ bars.py
@@ -1,8 +1,21 @@
 import json
-
+import codecs
 
 def load_data(filepath):
-    pass
+    text = None
+    try:
+        with codecs.open(filepath, "r", "utf_8_sig") as loading_file:
+            return loading_file.read()
+    except FileNotFoundError as ex:
+        print(ex)
+
+
+def load_from_json(filepath):
+    try:
+        with codecs.open(filepath, "r", "utf_8_sig") as loading_file:
+            return json.load(loading_file)
+    except FileNotFoundError as ex:
+        print(ex)
 
 
 def get_biggest_bar(data):
@@ -18,4 +31,5 @@ def get_closest_bar(data, longitude, latitude):
 
 
 if __name__ == '__main__':
-    pass
+    json_data = load_from_json("bars.json")
+    print(json_data)
