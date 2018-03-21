950a2e87c6653d3da78cd4dc027bffce0dc9ef3b
Ruslan <Rusiacugap@mail.ru>
2018-03-21 14:53:28 +0300

    Update bars.py
    Технически задача выполнена

--- bars.py
+++ bars.py
@@ -18,18 +18,35 @@ def load_from_json(filepath):
         print(ex)
 
 
-def get_biggest_bar(data):
-    pass
+def get_biggest_bar(json_data):
+    return max(json_data["features"], key=lambda feature: 
+               feature["properties"]["Attributes"]["SeatsCount"])
 
 
-def get_smallest_bar(data):
-    pass
+def get_smallest_bar(json_data):
+    return min(json_data["features"], key=lambda feature: 
+               feature["properties"]["Attributes"]["SeatsCount"])
+
+def get_distance_of_two_points(point_1, point_2):
+    return (point_1[0]-point_2[0])**2 + (point_1[1]-point_2[1])**2
 
 
-def get_closest_bar(data, longitude, latitude):
-    pass
+def get_closest_bar(json_data, longitude, latitude):
+    return min(json_data["features"], key=lambda feature: 
+	           get_distance_of_two_points(feature["geometry"]["coordinates"], 
+			                              [longitude, latitude]))
 
 
 if __name__ == '__main__':
     json_data = load_from_json("bars.json")
-    print(json_data)
+    smallest_bar = get_smallest_bar(json_data)
+    biggest_bar = get_biggest_bar(json_data)
+    print("Biggest bar is " + biggest_bar["properties"]["Attributes"]["Name"])
+    print("Smallest bar is " + smallest_bar["properties"]["Attributes"]["Name"])
+    
+    print("Please input your longitude:")
+    longitude = float(input())
+    print("Please input your latitude:")
+    latitude = float(input())
+    closest_bar = get_closest_bar(json_data, longitude, latitude)
+    print("Closest bar is " + closest_bar["properties"]["Attributes"]["Name"])
